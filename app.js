d888624e4a0ec9151a6b5e025298fa87d5fc6e69
daudi13 <davouma13@gmail.com>
2021-06-29 07:32:23 +0300

    commit js

---
+++ app.js
@@ -0,0 +1,43 @@
+const checkBtn = document.querySelector('.check');
+const againBtn = document.querySelector('.again');
+const guessNumber = document.querySelector('.guess').value;
+const numbre = document.querySelector('.number');
+const theScore = document.querySelector('.score');
+const theMessage = document.querySelector('.message').textContent;
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+let highscore = 0
+
+
+
+checkBtn.addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value)
+    console.log(guess)
+
+    if (!guess) {
+        displayMessage('No number!');
+    }
+
+    else if (guess === secretNumber) {
+        displayMessage('correct Number');
+    }
+
+    else if (guess !== secretNumber) {
+
+        if (score > 1) {
+            displayMessage(guess > secretNumber ? `too high!!` : `too low!!`);
+            score--;
+            document.querySelector('.score').innerHTML = score;
+        }
+        else {
+            displayMessage('you lost, try again');
+            theScore = 0;
+        }
+    }
+})
+
+function displayMessage(message) {
+    document.querySelector('.message').textContent = message
+}
